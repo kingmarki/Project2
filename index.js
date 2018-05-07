6f955d78ba8e830ab43a03b19e92c5adf8e36b98
jdwilsonfsu <jdwilson@frostburg.edu>
2018-05-07 14:54:18 -0400

    end day 3

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 import routes from './src/routes/routes';
 
 
 const app = express();
 const PORT = 3000;
+
+//mongo connection
+mongoose.connect('mongodb://localhost/project2');
+
+//bodyparser
+app.use(bodyParser.urlencoded({ extended: true }));//parse incoming requests with urlencoded payloads
+app.use(bodyParser.json());//parse incoming requests with json payloads
 
 routes(app);
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,17 @@
+import { addNewContact } from "../controllers/controller";
+
 const routes = (app) => {
     //Contact route (whole collection or posting to db)
     app.route('/contact')
-    .get((req,res) => {
-        res.send('GET request was sucessfull')
+    .get((req,res,next) => {
+        //middleware
+        console.log(`Request from: ${req.originalUrl}`)
+        console.log(`Request used ${req.method}`)
+        next();
+    }, (req,res) =>{
+        res.send('GET request sucessful')
     })
-    .post((req,res) => {
-        res.send('POST request was sucessfull')
-    });
+    .post(addNewContact);
 
     //specific id's in database
     app.route('/contact/:contactId')
